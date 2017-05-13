59908106e99e92fa46d9dfabc660ec75f98f6a89
nikhidas <nikhildasnomula@ymail.com>
2017-05-12 23:15:31 -0400

    Deleting nikhildasnomula from MEAN authapp as I moved it to a private repo

--- nikhildasnomula/config/database.js
+++
@@ -1,8 +0,0 @@
-const databaseUrl = 'mongodb://nikhil:123456@ds131041.mlab.com:31041/nikhildasnomula';
-const secret = 'yoursecret';
-
-module.exports = {
-  //database: 'mongodb://localhost:27017/nikhildasnomula',
-  database: databaseUrl,
-  secret: secret,
-}
